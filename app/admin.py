acee0e247de45b6fd8d3275e5bbf2a97cbb05ecf
reficul31 <reficul31@gmail.com>
2016-12-26 22:45:31 +0530

    Adding the admin views to the app.

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from app.models import User, Complaint
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Complaint)
+# admin.site.register(Post, MarkdownModelAdmin)
