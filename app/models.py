3f12566694b140d0aa93eca28700d9804a0b21b4
reficul31 <reficul31@gmail.com>
2016-12-26 22:45:46 +0530

    Changing the models

--- app/models.py
+++ app/models.py
@@ -7,23 +7,22 @@ class User(models.Model):
 	shares = models.IntegerField()
 	reg_data = models.DateField()
 
-	def __init__(self, username, password, shares, reg_data):
-		username = username
-		password = password
-		shares = shares
-		reg_data = reg_data
+	def __unicode__(self):
+		return self.username
 
 	def __unicode__(self):
 		return "<User %s>"%username
 
 class Complaint(models.Model):
-	user = models.ForeignKey()
+	user = models.ForeignKey(
+		'User'
+	)
 	type = models.TextField(max_length=500)
 	description = models.TextField()
 
-	def __init__(self, type, description):
-		type = type
-		description = description
+	def __unicode__(self):
+		return self.type
+
 
 	def __unicode__(self):
 		return "<Type %s>"%type
