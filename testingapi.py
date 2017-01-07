761ec1eaaa3ce5d600900c6a71558d940372f5d1
Nitin Sethi <nitinsethi.iitr@gmail.com>
2017-01-07 10:18:04 +0530

    finalapp

--- testingapi.py
+++ testingapi.py
@@ -1,21 +1,19 @@
+#Thos file is to test tradeoff analytics API by IBM
 import requests
 import json	
-#parameters = {"username" : 8c818e92-ce0e-40e4-8e5f-20ab9b839f1f, "password" : PFfXrQRiMAUY}
-#response = requests.get("https://gateway.watsonplatform.net/tradeoff-analytics/api/v1/dilemmas?generate_visualization=false", params=parameters)
-from requests.auth import HTTPBasicAuth
-# Print the status code of the response.
 from os.path import join, dirname
 from watson_developer_cloud import TradeoffAnalyticsV1
+#parameters = {"username" : 8c818e92-ce0e-40e4-8e5f-20ab9b839f1f, "password" : PFfXrQRiMAUY}
+#response = requests.get("https://gateway.watsonplatform.net/tradeoff-analytics/api/v1/dilemmas?generate_visualization=false", params=parameters)
+#above code does not work and returns empty binary
 
-
-
+from requests.auth import HTTPBasicAuth
 tradeoff_analytics = TradeoffAnalyticsV1(
+  username='servicecredentials-username',
+  password='servicecredentials-password')#create an instance
 
-  username='8c818e92-ce0e-40e4-8e5f-20ab9b839f1f',
-  password='PFfXrQRiMAUY')
 with open('/home/sethinitin4/Downloads/problem.json') as problem_json:
-#with open(os.path.join('/home/sethinitin4/Downloads', 'problem.json')) as problem_json:
+#enter your file path for json
   dilemma = tradeoff_analytics.dilemmas(json.load(problem_json),
                                         generate_visualization=False)
-
 print(json.dumps(dilemma, indent=2))
--- toneanalyzer.py
+++ toneanalyzer.py
@@ -1,16 +1,17 @@
-import requests
+#The tone analyzer API is used to analyze subjective queries
+#This portion needs to incorporated still and returned json will give anger and sadness values which will me minimized.
 import json	
 from requests.auth import HTTPBasicAuth
 from os.path import join, dirname
 from watson_developer_cloud import ToneAnalyzerV3
 
-
-
 tone_analyzer = ToneAnalyzerV3(
-   username='0b8745a9-77f9-444f-85aa-d6a02f09b853',
-   password='ifyDaK8rvt4F',
-   version='2016-05-19')
+   username='servicecredential-username',#enteryourusername
+   password='servicecredential-password',#enteryourpassword
+   version='2016-05-19')# creating an instance of the service
+
 with open('/home/sethinitin4/Downloads/test_tone.txt') as file:
-  dilemma = tone_analyzer.tone(file)
+  dilemma = tone_analyzer.tone(file)#your text file
 
 print(json.dumps(tone_analyzer.tone(text='A word is dead when it is said, some say. Emily Dickinson'), indent=2))
+#fit in your file in place of text for analysis
